22c23a9f1ac0b00db75c800406ea0572cb2f918c
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-08-04 00:25:06 -0400

    styling breaks my button, function over form

--- client/src/components/HomeFeed.js
+++ client/src/components/HomeFeed.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { CurrentUserContext } from "../components/CreateUserContext";
 import TweetDetails from "../tweet/TweetDetails";
 import TweetInput from "../tweet/TweetInput";
+import TweetInput2 from "../tweet/TweetInput2";
 
 const HomeFeed = () => {
   const { currentUser, status } = useContext(CurrentUserContext);
@@ -28,10 +29,14 @@ const HomeFeed = () => {
     <>
       {homeStatus === "idle" ? (
         <HomeBox>
-          <TweetInput
+          <TweetInput2
             homeFeedData={homeFeedData}
             setHomeFeedData={setHomeFeedData}
-          />
+          ></TweetInput2>
+          {/* // <TweetInput
+          //   homeFeedData={homeFeedData}
+          //   setHomeFeedData={setHomeFeedData}
+          // /> */}
           {homeFeedData.tweetIds.map((tweetId) => {
             let individualTweet = homeFeedData.tweetsById[tweetId];
 
---
+++ client/src/tweet/TweetInput2.js
@@ -0,0 +1,73 @@
+import React, { useState, useContext, useEffect } from "react";
+
+import styled from "styled-components";
+import { CurrentUserContext } from "../components/CreateUserContext";
+
+const TweetInput2 = ({ setHomeFeedData, homeFeedData }) => {
+  const [textBox, setTextBox] = React.useState("meow here!");
+  const [charLimit, setCharLimit] = React.useState(280);
+  console.log(textBox);
+  const minusChars = textBox.length;
+
+  const Total = styled.div`
+    display: flex;
+  `;
+  const Input = styled.input`
+    border-radius: 4px;
+    border: 1px solid grey;
+    margin-left: 8vw;
+    margin-bottom: 50px;
+    margin-top: 50px;
+    height: 50px;
+    width: 500px;
+  `;
+  const Button = styled.button`
+    margin-left: 20px;
+    color: purple;
+    background-color: coral;
+    border-radius: 4px;
+    margin-top: 50px;
+    width: 100px;
+    height: 50px;
+  `;
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ status: textBox }),
+  };
+
+  function sendTweet(param) {
+    fetch("/api/tweet", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        fetch(`/api/me/home-feed`)
+          // We get the API response and receive data in JSON format...
+          .then((response) => response.json())
+          // ...then we update the users state
+          .then((data) => {
+            setHomeFeedData(data);
+          });
+      });
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={textBox}
+        onChange={(ev) => setTextBox(ev.target.value)}
+        onKeyDown={(ev) => {
+          switch (ev.key) {
+            case "Enter": {
+              sendTweet(textBox);
+              return;
+            }
+          }
+        }}
+      />
+      <button onClick={() => sendTweet(textBox)}>Meow</button>
+      <div>{charLimit - minusChars}</div>
+    </div>
+  );
+};
+export default TweetInput2;
