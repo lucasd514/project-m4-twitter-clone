e0b00b92c15432e1c5936d5b6da3a65d3aad173e
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-08-04 21:55:56 -0400

    addded DDR

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -2,7 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const Notifications = () => {
-  return <div>hello world this is notification</div>;
+  return (
+    <div>
+      <img src="https://cdn.forzaitalianfootball.com/wp-content/uploads/2019/04/De-Rossi-Zaniolo-Roma-Sampdoria-1000x0-c-default.webp" />
+    </div>
+  );
 };
 
 export default Notifications;
