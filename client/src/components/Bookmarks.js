776bb522aa8f2fad5cc509ba630626b45d614f32
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-08-05 13:20:42 -0400

    updated color change

--- client/src/components/Bookmarks.js
+++ client/src/components/Bookmarks.js
@@ -2,7 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const Bookmarks = () => {
-  return <div>hello world this is bookmarks</div>;
+  return (
+    <div>
+      <img src="https://laroma24.it/wp-content/uploads/2018/04/BIG-roma-barcellona-3-0.jpeg" />
+    </div>
+  );
 };
 
 export default Bookmarks;
--- client/src/tweet/TweetInput2.js
+++ client/src/tweet/TweetInput2.js
@@ -3,6 +3,8 @@ import React, { useState, useContext, useEffect } from "react";
 import styled from "styled-components";
 import { CurrentUserContext } from "../components/CreateUserContext";
 
+const maxNum = document.getElementById("limit");
+
 const TweetInput2 = ({ setHomeFeedData, homeFeedData }) => {
   const [textBox, setTextBox] = React.useState("meow here!");
   const [charLimit, setCharLimit] = React.useState(280);
@@ -45,13 +47,35 @@ const TweetInput2 = ({ setHomeFeedData, homeFeedData }) => {
         }}
       />
       <button onClick={() => sendTweet(textBox)}>Meow</button>
-      <div>{charLimit - minusChars}</div>
+      {280 - textBox.length > 150 ? (
+        <Black>{280 - textBox.length}</Black>
+      ) : 280 - textBox.length > 50 ? (
+        <Yellow>{280 - textBox.length}</Yellow>
+      ) : 280 - textBox.length >= 0 ? (
+        <Red>{280 - textBox.length}</Red>
+      ) : (
+        <Red></Red>
+      )}
     </div>
   );
 };
 
+const Black = styled.div`
+  color: black;
+`;
+const Red = styled.div`
+  color: rgb(134, 38, 51);
+`;
+const Yellow = styled.div`
+  color: rgb(240, 188, 66);
+`;
+
 const Total = styled.div`
   display: flex;
+`;
+
+const CharBox = styled.div`
+  color: blue;
 `;
 const TweetInput = styled.input`
   border-radius: 4px;
